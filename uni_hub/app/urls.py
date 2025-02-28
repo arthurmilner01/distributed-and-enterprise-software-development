3b74ad9d9619496c995b05810c90babd4d49db3f
Rohaan Aslam <rohaan2.aslam@live.uwe.ac.uk>
2025-02-28 15:40:48 +0000

    removed / in refresh url

--- uni_hub/app/urls.py
+++ uni_hub/app/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = [
     path("auth/jwt/create/", CustomTokenObtainPairView.as_view(), name="jwt-create"),
-    path("auth/jwt/refresh/", CustomTokenRefreshView.as_view(), name="jwt-refresh"),
+    path("auth/jwt/refresh", CustomTokenRefreshView.as_view(), name="jwt-refresh"),
     path('user/update/<int:user_id>/', UserProfileUpdateView.as_view(), name='user-profile-update'),
     path("protected/", protected_view, name="protected-view"),
     path("event/", event_view, name="event-view"),
